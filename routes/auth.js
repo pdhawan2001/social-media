0f7c02921e513e8fcc75cbb55d86c4edf1fadd24
Parth Dhawan <pdhawan2001@gmail.com>
2022-02-09 22:04:43 +0530

    completed project

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,38 @@ const User = require('../models/users');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  res.send('')
+router.post('/register', async (req, res) => {
+  try {
+    // generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password);
+
+    // create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
